e0ad358477bd665b29619d5c0b20a741130fbda3
codeForMobile <code.mobile@protonmail.com>
2024-08-26 20:00:06 +0530

    display company page

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,20 +1,25 @@
-import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import { useParams } from 'react-router'
+import { useEffect, useState } from 'react'
+import { getCompany } from '../lib/graphql/queries'
 
 function CompanyPage() {
-  const { companyId } = useParams();
+  const { companyId } = useParams()
+  const [company, setCompany] = useState()
 
-  const company = companies.find((company) => company.id === companyId);
+  useEffect(() => {
+    getCompany(companyId).then(setCompany)
+  }, [companyId])
+
+  if (!company) {
+    return <div>Loading Company....</div>
+  }
+
   return (
     <div>
-      <h1 className="title">
-        {company.name}
-      </h1>
-      <div className="box">
-        {company.description}
-      </div>
+      <h1 className="title">{company.name}</h1>
+      <div className="box">{company.description}</div>
     </div>
-  );
+  )
 }
 
-export default CompanyPage;
+export default CompanyPage
