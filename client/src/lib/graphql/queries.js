a331a6d5aa6e4d8b6becb3e32eb0f4e3aaf56da3
codeForMobile <code.mobile@protonmail.com>
2024-08-26 18:08:11 +0530

    display single job on ui, integrate with be

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -2,21 +2,40 @@ import { GraphQLClient, gql } from 'graphql-request'
 
 const client = new GraphQLClient('http://localhost:9000/graphql')
 
-export async function getJobs () {
-    const query = gql`
-        query {
-          jobs {
-            id
-            title
-            date
-            company {
-                id
-                name
-                }
-            }
+export async function getJob(id) {
+  const query = gql`
+    query ($id: ID!) {
+      job(id: $id) {
+        id
+        date
+        title
+        company {
+          id
+          name
         }
-    `
+        description
+      }
+    }
+  `
+  const { job } = await client.request(query, { id })
+  return job
+}
 
-    const { jobs } = await client.request(query)
-    return jobs
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        title
+        date
+        company {
+          id
+          name
+        }
+      }
+    }
+  `
+
+  const { jobs } = await client.request(query)
+  return jobs
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,25 +1,20 @@
-import { useEffect, useState } from 'react';
-import JobList from '../components/JobList';
-import { getJobs } from '../lib/graphql/queries';
-
-getJobs().then((job) => console.log(job))
+import { useEffect, useState } from 'react'
+import JobList from '../components/JobList'
+import { getJobs } from '../lib/graphql/queries'
 
 function HomePage() {
-
-  const [jobs, setJobs]= useState([])
+  const [jobs, setJobs] = useState([])
 
   useEffect(() => {
     getJobs().then(setJobs)
   }, [])
-  
+
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
